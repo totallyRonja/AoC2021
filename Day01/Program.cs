b9bffc766ec360d7dbc309021384cac70db8d92b
Ronja <git@ronja.space>
2021-12-01 16:19:32 +0100

    abuse my libarary some more ^^

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,7 +1,7 @@
 ﻿using RonjasToolbox;
 
 var input = File.ReadAllLines("./input.txt");
-var values = input.Select(str => int.Parse(str)).ToArray();
+var values = input.Select(int.Parse).ToArray();
 
 Console.WriteLine("Day 01");
 Console.WriteLine("Part 1");
@@ -9,7 +9,7 @@ Console.WriteLine("Part 1");
 {
 	//I'm addicted to LINQ
 	var increases = values
-		.Zip(values.Skip(1))
+		.Pair()
 		.Count(pair => pair.Second > pair.First);
 	Console.WriteLine(increases);
 }
@@ -17,7 +17,7 @@ Console.WriteLine("Part 1");
 {
 	//non-overcomplicated for
 	var increases = 0;
-	for(int i = 1; i<input.Length; i++) {
+	foreach(int i in 1..input.Length) {
 		if (values[i] > values[i - 1]) increases++;
 	}
 	Console.WriteLine(increases);
@@ -28,7 +28,7 @@ Console.WriteLine("Part 2");
 {
 	//Ronja thinks she's smort and overcomplicates things
 	var increases = 0;
-	for(int i = 1; i<input.Length - 2; i++) {
+	foreach(int i in 1..(input.Length-2)) {
 		//compare the value that leaves the window (-1) with the value that enters the window (+2)
 		if (values[i+2] > values[i - 1]) increases++;
 	}
@@ -38,7 +38,7 @@ Console.WriteLine("Part 2");
 {
 	//one more LINQ solution as a treat
 	var increases = values
-		.Zip(values.Skip(1), values.Skip(2))
+		.Triplet()
 		.Select(window => window.First + window.Second + window.Third)
 		.Pair()
 		.Count(pair => pair.Second > pair.First);
