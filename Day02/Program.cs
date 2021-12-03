7d72833ecf218015922e5b6d794da17b40f15624
Ronja <git@ronja.space>
2021-12-03 09:03:20 +0100

    Day03

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -46,6 +46,22 @@ var program = input.Select(line => {
 	Console.WriteLine($"second sub: goal position: {pos} | product: {pos.X * pos.Y}");
 }
 
+{
+	//the whole thing in a single line/semicolon FOR SOME REASON
+	Console.WriteLine(File.ReadAllLines("./input.txt")
+		.Select(input => (Type: input.Split(" ")[0], Amount: int.Parse(input.Split(" ")[1])))
+		.Aggregate((X:0, Y:0, Aim: 0, Result: 0), (source, instruction) => instruction.Type switch{
+			"forward" => (source.X+instruction.Amount, 
+				source.Y + source.Aim * instruction.Amount, 
+				source.Aim, source.X+instruction.Amount * (source.Y + source.Aim * instruction.Amount)),
+			"down" => (source.X, source.Y, 
+				source.Aim + instruction.Amount, source.Result),
+			"up" => (source.X, source.Y, 
+				source.Aim - instruction.Amount, source.Result),
+			_ => throw new ArgumentOutOfRangeException(),
+		}).Result);
+}
+
 enum Command {
 	Forward,
 	Up,
---
+++ Day03/Program.cs
@@ -0,0 +1,42 @@
+﻿using RonjasToolbox;
+
+using static Functions;
+
+var input = File.ReadAllLines("input.txt");
+
+var numbers = input.Select(input => Convert.ToInt16(input, 2)).ToArray();
+
+var gamma = 0;
+foreach (int i in 0..12) {
+	if (MostCommonBit(numbers, i)) gamma |= 1 << i;
+}
+var epsilon = (~gamma) & 0x0FFF;
+
+Console.WriteLine($"gamma: {gamma} - epsilon: {epsilon} - power_consumption: {gamma * epsilon}");
+
+var numberMap = new HashSet<short>(numbers);
+foreach (var i in (0..12).Iter().Reverse()) {
+	var mostCommonBit = MostCommonBit(numberMap, i);
+	numberMap.RemoveWhere(number => (((number >> i) & 1) == 1) != mostCommonBit);
+	if(numberMap.Count <= 1) break;
+}
+var oxygenRating = numberMap.First();
+
+numberMap = new HashSet<short>(numbers);
+foreach (var i in (0..12).Iter().Reverse()) {
+	var mostCommonBit = !MostCommonBit(numberMap, i);
+	numberMap.RemoveWhere(number => (((number >> i) & 1) == 1) != mostCommonBit);
+	if(numberMap.Count <= 1) break;
+}
+var co2Scrubber = numberMap.First();
+
+Console.WriteLine($"oxygen: {oxygenRating} - scrubber: {co2Scrubber} - life support: {oxygenRating * co2Scrubber}");
+
+
+public static class Functions {
+	public static bool MostCommonBit(IEnumerable<short> numbers, int position) {
+		var trueBits = numbers.Count(num => ((num >> position) & 1) == 1);
+		var falseBits = numbers.Count() - trueBits;
+		return trueBits >= falseBits;
+	}
+}
